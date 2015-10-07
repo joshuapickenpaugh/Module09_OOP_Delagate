5272f02453120e652bb0b90cbb34f3f8275df0bf
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-07 16:53:19 -0500

    !

--- Modulue08_OOP_Delagate/Program.cs
+++ Modulue08_OOP_Delagate/Program.cs
@@ -7,17 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+public delegate void Del(string message);
+
 namespace Modulue08_OOP_Delagate
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            
-            
-            
             Console.ReadKey();
+        }
+
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
