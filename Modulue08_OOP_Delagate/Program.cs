d1c22556acce99f0847fcdfae516f1b719d46620
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2015-10-07 16:43:54 -0500

    First Commit

---
+++ Modulue08_OOP_Delagate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulue08_OOP_Delagate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
